1a00abbea8bf2688d312aa3618d70bc23289f936
susu <1123844210@qq.com>
2018-07-26 16:26:00 +0800

    登录数据验证

--- controller/user.js
+++ controller/user.js
@@ -1,67 +1,67 @@
 //路由回调函数的处理
 //导入mysql
-// const mysql = require('mysql');
+const mysql = require('mysql');
 
-// //配置数据库
-// var connection = mysql.createConnection({
-//     host: 'localhost', // 要连接的主机名
-//     user: 'root', // 要连接的数据库的用户名
-//     password: 'root', // 数据库密码
-//     database: 'newssql' // 数据库
-// });
-// connection.connect();
+//配置数据库
+var connection = mysql.createConnection({
+    host: 'localhost', // 要连接的主机名
+    user: 'root', // 要连接的数据库的用户名
+    password: 'root', // 数据库密码
+    database: 'newssql' // 数据库
+});
+connection.connect();
 
 
 exports.showSignIn = (req,res)=>{
 	res.render('signin.html');
 }
 
-// exports.handleSignIn = (req,res)=>{
-// // console.log(req)
-// 	//获取表单数据
-// 	const body = req.body;
+exports.handleSignIn = (req,res)=>{
+// console.log(req)
+	//获取表单数据
+	const body = req.body;
 
-// 	// console.log(req.body);
+	// console.log(req.body);
 
-// 	//数据库操作
+	//数据库操作
 	
-// 	const sqlstr = 'SELECT * FROM users WHERE email = ?';
+	const sqlstr = 'SELECT * FROM users WHERE email = ?';
 
-// 	connection.query(sqlstr,body.email,(err,results)=>{
+	connection.query(sqlstr,body.email,(err,results)=>{
 
-// 		if(err) {
-// 			return res.send({
-// 				code: 1,
-// 				message:err.message
-// 			})
-// 		}
-// 		// console.log(results);
-// 		//是否为空
-// 		if(results.length == 0) {
+		if(err) {
+			return res.send({
+				code: 1,
+				message:err.message
+			})
+		}
+		// console.log(results);
+		//是否为空
+		if(results.length == 0) {
 
-// 			return res.send({
-// 				code: 404,
-// 				message:'邮箱不存在'
-// 			})
+			return res.send({
+				code: 404,
+				message:'邮箱不存在'
+			})
 
-// 		} 
-// 		//不为空的话，验证密码
-// 		if(results[0].password != body.password) {
+		} 
+		//不为空的话，验证密码
+		if(results[0].password != body.password) {
 
-// 			return res.send({
-// 				code: 1,
-// 				message:'密码错误'
-// 			})
+			return res.send({
+				code: 2,
+				message:'密码错误'
+			})
 
-// 		}
-// 		//走到这里，说明密码正确//密码正确，进行跳转
-// 		return res.send({
-// 			code:200,
-// 			message:'可以登录'
-// 		})
+		}
+		//走到这里，说明密码正确//密码正确，进行跳转
+		res.send({
+			code:200,
+			message:'可以登录'
+		})
 		
-// 	})
+	})
 
 
 
-// }
+}
--- router.js
+++ router.js
@@ -10,6 +10,6 @@ const router = express.Router();
 	//显示登录页面
 router.get('/signin',user.showSignIn);
 	//校验表单
-// router.post('/signin',user.handleSignIn);	
+router.post('/signin',user.handleSignIn);	
 //导出router
 module.exports = router;
